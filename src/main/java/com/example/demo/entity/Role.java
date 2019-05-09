5b0696e71beb485f3ec0da4fd7dc79096adb8538
leo <lichaofeng1991@gmail.com>
2019-05-09 17:02:27 +0800

    ”第一次提交

---
+++ src/main/java/com/example/demo/entity/Role.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+public class Role {
+
+    private Long id;
+    private String role;
+    private String promisson;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPromisson() {
+        return promisson;
+    }
+
+    public void setPromisson(String promisson) {
+        this.promisson = promisson;
+    }
+}
