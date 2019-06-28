43aef061199e6206b42e121333dd7d20aa993028
leo <lichaofeng1991@gmail.com>
2019-06-28 13:55:12 +0800

    身份验证

---
+++ src/test/java/com/example/demo/chapter2/AuthenticatorTest.java
@@ -0,0 +1,57 @@
+package com.example.demo.chapter2;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.UUID;
+
+
+public class AuthenticatorTest {
+
+
+    @Test(expected = UnknownAccountException.class)
+    public void testAllSuccessfulStrategyWithFail() {
+        login("classpath:chapter2/shiro-authenticator-all-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+    }
+
+    @Test
+    public void testAllSuccessfulStrategyWithSuccess() {
+        login("classpath:chapter2/shiro-authenticator-all-success.ini");
+        Subject subject = SecurityUtils.getSubject();
+
+        //得到一个身份集合，其包含了Realm验证成功的身份信息
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        Assert.assertEquals(2, principalCollection.asList().size());
+    }
+
+    private void login(String configFile) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        subject.login(token);
+    }
+@Test
+    public void test(){
+    String fileName = String.valueOf(System.currentTimeMillis());
+        System.out.println(fileName);
+    }
+}
